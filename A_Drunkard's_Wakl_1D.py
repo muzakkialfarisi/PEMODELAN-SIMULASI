1498ceb2a8f1a5bcca4575b301b198dfa99e6f26
Muzakki Ahmad Al Farisi <46997136+muzakkialfarisi@users.noreply.github.com>
2020-05-01 19:52:31 +0700

    Update A_Drunkard's_Wakl_1D.py

--- A_Drunkard's_Wakl_1D.py
+++ A_Drunkard's_Wakl_1D.py
@@ -1,19 +1,26 @@
 import random
 import matplotlib.pyplot as plt
 
+# inisialisasi berapa banyak langkah yang akan diuji dalam 1 kali simulasi
 N_steps = 200
 
+# inisialsisasi panjang per 1 kali langkah
 range_step = 1
 
+# inisialisasi besar graph yang akan di analisis
 fig, ax = plt.subplots(figsize=(10, 8))
 ax.set_xlim(0, 25.1)
 ax.set_ylim(0, 25.1)
 
+# inisialisasi titik langkah awal
 x, y = [], []
 x.append(random.randint(1,25))
 y.append(random.randint(1,25))
 
+# inisialisasi kemana arah langkah selanjtnya
 next_step = ['xplus', 'yplus']
+
+# perulangan untuk N_step
 for i in range(N_steps-1):
   ax.scatter(x[i],y[i], c='k')
   plt.pause(0.1)
